3f1f8707b9383ecf47b837fc54d11c9628e9ec57
gerlinp <gerlin.pierrelouis@gmail.com>
2021-07-17 18:29:22 -0400

    searchbar

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const search = document.querySelector('.search-container');
+const searchBar = document.querySelector('#search')
+
+search.innerHTML = `
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>
+`;
+
